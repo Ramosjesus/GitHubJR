06c692cf5ae88c2e356bddf0d4238de33cb6b412
Ramosjesus <jramosa999@alumnos.imf.com>
2024-04-11 13:08:58 +0200

    3er Commit

--- Ejercicio.java
+++ Ejercicio.java
@@ -5,5 +5,6 @@ public class Ejercicio
   public static void main( String[] args )
   {
     System.out.println( "Hola " ) ;
+    System.out.println( "hola guapo ." ) ;
   }
 }
---
+++ ejercicioIgnorado.java
@@ -0,0 +1,9 @@
+package repasoPractica;
+
+public class ejercicioIgnorado
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "Ignorame por fa" ) ;
+  }
+}
